7c00d620814466f6bd86682f15437701bc549a4c
Manases Lovera <manaseslovera07@gmail.com>
2024-09-22 23:23:38 -0400

    feat: booking schema

---
+++ backend/src/db/booking.db.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const BookingSchema = new Schema({
+    clientId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    serviceId: { type: Schema.Types.ObjectId, ref: 'Service', required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    status: { type: String, enum: ['Pending', 'Confirmed', 'Cancelled'], default: 'Pending' },
+    adminNotes: { type: String },
+    clientNotes: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+  });
